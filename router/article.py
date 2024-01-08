751245d5b76e74df565ce91bfaf08222311c1327
Shantanu Singh <shantan.singh@gmail.com>
2024-01-07 21:31:12 -0800

    Adding Authentication

---
+++ router/article.py
@@ -0,0 +1,23 @@
+from typing import List  # noqa
+from schema import ArticleBase, ArticleDisplay, UserBase  # noqa
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from db.database import get_db
+from db import db_article
+from auth.oauth2 import oauth2_schema
+
+router = APIRouter(prefix="/article", tags=["article"])
+
+
+# Create artiucle
+@router.post("/", response_model=ArticleDisplay)
+def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+    return db_article.create_article(db, request)
+
+
+# get speific article
+@router.get("/{id}")  # , response_model = ArticleDisplay)
+def get_article(
+    id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_schema)
+):  # Depends(oauth2_schema secures the api)
+    return {"data": db_article.get_article(db, id)}
